9de458443a874d135d647356589164f5d7addf42
keven <kewen@unisound.com>
2020-02-02 13:16:21 +0800

    init

---
+++ xiaoguan.js
@@ -0,0 +1,60 @@
+const Koa = require("koa");
+const fs = require("fs");
+const router = require('koa-router')();
+const formidable = require("formidable");
+const app = new Koa();
+const co = require('co');
+const OSS = require('ali-oss');
+const client = new OSS({
+    region: 'oss-cn-beijing', //自定义项
+    accessKeyId: 'LTAIviSWChYCOhKi', //自定义项
+    accessKeySecret: 'anLuQmxRsS5uV7oIzx4wE4dCLWvzP1' //自定义项
+});
+
+const home = (ctx, next) => {
+    ctx.body = require("fs").readFileSync("./tpl/xiaoguan.html", "utf-8");
+}
+
+const upfile = async(ctx, next) => {
+    var alioss_upfile = function() {
+        return new Promise(function(resolve, reject) {
+            var form = new formidable.IncomingForm();
+            form.parse(ctx.req, function(err, fields, files) {
+                if (err) { throw err; return; }
+                // 文件名
+                var date = new Date();
+                var Month = date.getMonth()*1+1 < 10 ? '0'+(date.getMonth()*1 + 1) : date.getMonth()*1 + 1;
+                var strDate = date.getDate() < 10 ? '0'+date.getDate() : date.getDate()
+                var time = '' + date.getFullYear() + Month + strDate;
+                var filepath = time + '/' + date.getTime();
+                var fileext = files.file.name.split('.');
+                var upfile = files.file.path;
+                // var upfile = '8.jpg';
+                var newfile = filepath + '.' + fileext[1];
+                console.log('upfile: ', upfile)
+                console.log('newfile: ', newfile)
+                //ali-oss
+                co(function*() {
+                    client.useBucket('liz520'); //自定义项
+                    var result = yield client.put(newfile, upfile);
+                    //var result = yield client.put(fields.store, new Buffer(fields.buffer));
+                    console.log('文件上传成功!', result.url);
+                    ctx.response.type = 'json';
+                    ctx.response.body = result.url;
+                    resolve(next());
+                }).catch(function(err) {
+                    console.log(err);
+                });
+            });
+        });
+    };
+    await alioss_upfile();
+};
+
+router.get('/', home);
+
+router.post('/upfile', upfile);
+
+app.use(router.routes());
+
+app.listen(3001);
